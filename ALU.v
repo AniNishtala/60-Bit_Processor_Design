365c486b0c32c4efa443fb05afa40430f7664ed1
jayjayayala8 <168233582+jayjayayala8@users.noreply.github.com>
2024-12-05 11:36:39 -0800

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -1,20 +1,20 @@
 module ALU(
-  input clk,
-  input [2:0] op,
-  input [59:0] A, B,
-  output [59:0] C
+	input clk,
+	input [2:0] op,
+	input [59:0] A, B, 
+	output reg [59:0] C
 );
-
-always@(posedge clk)begin
-case(op)
-  0: C = A + B;
-  1: C = A - B;
-  2: C = A * B;
-  3: //Other operations!
-  4:
-  5:
-  6:
-  7:
-endcase
-end
-endmodule
+	always@(posedge clk) begin
+	case(op)
+	0: C = A + B;
+	1: C = A - B;
+	2: C = A * B;
+	3: C = A << 1;
+	4: C = A >> 1;
+	5: C =(A == B) ? 60'b1 : 60'b0;
+	6: C = (A > B) ? 60'b1 : 60'b0;
+	7: C = (A < B) ? 60'b1: 60'b0;
+	default: C = 60'b0;
+	endcase	
+	end
+endmodule 
