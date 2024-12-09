dc36c22d1c6da43c23792429e1ecb0e8f8861066
AnirudhNishtala <71745540+AniNishtala@users.noreply.github.com>
2024-12-09 11:27:40 -0800

    Create Multiplexer2-1.v

---
+++ Multiplexer2-1.v
@@ -0,0 +1,15 @@
+module Multiplexer21(
+
+	input wire [71:0] A,
+	input wire [71:0] B,
+	input wire selectionLine,
+	output reg [71:0] C
+);
+
+//when selectionLine is 1, output wire B
+
+	always @(*) 
+	     begin
+	assign C = (selectionLine) ? B : A; 
+end
+endmodule
