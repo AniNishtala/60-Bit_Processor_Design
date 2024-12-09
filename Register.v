b5e12b04c9938720617205ee7f333b54a8ace7d7
AnirudhNishtala <71745540+AniNishtala@users.noreply.github.com>
2024-12-09 12:25:07 -0800

    Update Register.v

--- Register.v
+++ Register.v
@@ -1,28 +1,38 @@
 module Register(
-	/**************************************************************************/
-	/*Might have to change data width with respect to ptoential future changes*/
-	/**************************************************************************/
-	input write, clk,
-	input [6-1: 0] reg1_address, reg2_address, reg_r_address,
-	input [72-1:0] result_in,	
-	output reg [72-1:0] data_out1, data_out2
+    input write, clk, reset,
+    input [9:0] reg1_address, reg2_address, reg_r_address,   // 10-bit address (to access 1024 registers)
+    input [71:0] result_in,
+    output reg [71:0] data_out1, data_out2
 );
 
-parameter n = 72; //data width
-parameter d = 6; //data address width
+    parameter n = 72;  // Data width (72 bits)
+    parameter d = 1024;  // Number of registers (1024 registers)
+    integer i;
 
-/*Need a way to access memory file here*/
-reg [n-1:0] datamemory [0:d-1]; 
-/***************************************/
+    // Register memory array to hold 1024 registers, each 72 bits
+    reg [n-1:0] datamemory [0:d-1];
 
+    // Initial block to read memory contents from a file (data_mem.txt)
+    initial begin
+        $readmemh("data_mem.txt", datamemory);  // Read 1024 entries from the file in hexadecimal format
+    end
 
-always @(posedge clk) begin
-	if(write) begin //If we are writing result 
-	datamemory[reg_r_address] <= result_in; //writing result in from ALU op into destination register address
-	end
-	else begin //else we are reading reg from memory and outputting into ALU
-	data_out1 <= datamemory[reg1_address];
-	data_out2 <= datamemory[reg2_address];
-	end 
-end 
+    // Write or read from registers
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            // Optionally clear the registers on reset
+            
+            for (i = 0; i < d; i = i + 1) begin
+                datamemory[i] <= 72'b0;
+            end
+        end else if (write) begin
+            // Write the result_in value to the specified register
+            datamemory[reg_r_address] <= result_in;
+        end else begin
+            // Read the data from the specified registers
+            data_out1 <= datamemory[reg1_address];
+            data_out2 <= datamemory[reg2_address];
+        end
+    end
+
 endmodule
