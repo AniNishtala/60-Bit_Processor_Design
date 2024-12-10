17c69e4dbd0e344a7430ff3b9d0e2ee88a99f9ff
jayjayayala8 <168233582+jayjayayala8@users.noreply.github.com>
2024-12-10 10:37:50 -0800

    Update ProgramCounter.v

--- ProgramCounter.v
+++ ProgramCounter.v
@@ -3,8 +3,9 @@ module ProgramCounter(
 	input reset,
 	input [54:0] jump_address,
 	input [67:0] branch_address,
-	input Jump_en, Branch_en
-	output reg [71:0] pc
+	input [71:0] PC_in,
+	input Jump_en, Branch_en,
+	output reg [71:0] pc_out
 );
 
 always @(posedge clk or posedge reset) begin
