7821f2ea4750d6fd8e403043b175d3e9252276e6
jayjayayala8 <168233582+jayjayayala8@users.noreply.github.com>
2024-12-10 10:25:45 -0800

    Update ProgramCounter.v
    Changed it with respect to our 72 bit since it was still on 60 bit

--- ProgramCounter.v
+++ ProgramCounter.v
@@ -1,9 +1,10 @@
 module ProgramCounter(
-input clk,
-input reset,
-input [59:0] jump_address,
-input jump_enable,
-output reg [59:0] pc
+	input clk,
+	input reset,
+	input [54:0] jump_address,
+	input [67:0] branch_address,
+	input Jump_en, Branch_en
+	output reg [71:0] pc
 );
 
 always @(posedge clk or posedge reset) begin
@@ -12,6 +13,8 @@ always @(posedge clk or posedge reset) begin
 		pc <= 0;
 	else if(jump_enable == 1)
 		pc <= jump_address;
+	else if(branch_en == 1)
+		pc <= branch_address
 	else
 		pc <= pc + 120; // increment by 120 bits to fetch two instructions at a time
 	end
