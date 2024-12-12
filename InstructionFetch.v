b6603f65802280126bad2de1780bb8db0488c463
AnirudhNishtala <71745540+AniNishtala@users.noreply.github.com>
2024-12-11 20:21:18 -0800

    Update InstructionFetch.v

--- InstructionFetch.v
+++ InstructionFetch.v
@@ -1,17 +1,17 @@
 //we need a txt file to read data memory from and instructions.
 module InstructionFetch(
-	input clk, reset,
+
 	input	wire[71:0]	address,  // instruction address
 	output 	reg [71:0]	instruction   // instruction
 	
 );
 
-  reg[71:0]  instruction_memory[0:1023]; 
+	reg[71:0]  instruction_memory[0:1023];
 
 	initial $readmemb ("machinecode.txt", instruction_memory);	// read test assembly code file
 
-	always @ (@posedge clk) begin
-		instruction <= instruction_memory[address[71:3]];
+always @ (*) begin
+		instruction <= instruction_memory[address[71:0]];
 end
 
 endmodule
