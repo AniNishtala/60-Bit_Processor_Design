a46acc0007e0bbc927e838e83f7e34f6af5b4526
jayjayayala8 <168233582+jayjayayala8@users.noreply.github.com>
2024-12-12 13:33:25 -0800

    Update Concatinator_tb.v

--- Concatinator_tb.v
+++ Concatinator_tb.v
@@ -2,7 +2,7 @@
 module Concatinator_tb;
     // Input and Output
     wire[71:0] concatinator_out;
-    reg [47:0] concatinator_in;
+	reg [49:0] concatinator_in;
     // Instantiate the Unit Under Test (UUT)
 	Concatinator concat(
 		.A(concatinator_in),
@@ -12,10 +12,10 @@ module Concatinator_tb;
     initial begin
         // Initialize clock, reset, and signals
         #50;
-	concatinator_in = 47'd70;
+	concatinator_in = 49'd70;
 
 	#50;
-	concatinator_in = 47'd48;
+	concatinator_in = 49'd48;
 
         // Check read value from register 1
         $display(concatinator_out);
